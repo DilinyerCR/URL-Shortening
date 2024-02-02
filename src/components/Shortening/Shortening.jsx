08cd9363feace8867004a12106e3161ced9e6b22
DilinyerCR <dilinyerecr@gmail.com>
2024-02-02 16:21:54 -0400

    commented

--- src/components/Shortening/Shortening.jsx
+++ src/components/Shortening/Shortening.jsx
@@ -1,48 +1,60 @@
 import styles from './Shortening.module.css'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 
 const Shortening = () => {
+  // * API Key para la API de TinyURL
   const API_KEY = "MSrganNsFvoTFs4DTdYxDzWc4yOXNiVoGIQeYbd8Ho8uQWbg7LKWTmyFSu0J"
 
-    // * Se crea un estado local para guardar el valor del input
-    const [ inputValue, setInputValue ] = useState('')
+  // ! Se definen los estados locales del componente.
+  // * Estado para almacenar la URL introducida por el usuario.
+  const [inputValue, setInputValue] = useState('');
 
-    // * Se crea un estado local para guardar el valor que proviene de la API, es decir la URL cortada
-    const [tinyUrl, setTinyUrl] = useState("");
+  // * Estado para almacenar la URL acortada.
+  const [tinyUrl, setTinyUrl] = useState('');
 
-    
-    const [links, setLinks] = useState([]);
+  // * Estado para almacenar una lista de todos los enlaces acortados.
+  const [links, setLinks] = useState([]);
 
-    // * Esta funcion captura lo que se escriba en el input y guarda ese valor en el estado local "inputValue"
+  // * Estado para mostrar mensajes de error al usuario.
+  const [error, setError] = useState(null);
+
+  // * Estado para almacenar el texto de los botones de copia.
+  const [buttonTexts, setButtonTexts] = useState([]);
+
+  // * Estado para indicar el índice del enlace copiado.
+  const [copiedIndex, setCopiedIndex] = useState(null);
+
+
+    // * Función para actualizar el estado `inputValue` con la URL introducida por el usuario.
     const handleInputChange  = (e) => {
       setInputValue(e.target.value)
       setError(null);
     }
 
 
-    const [error, setError] = useState(null);
-
-    const [buttonTexts, setButtonTexts] = useState([]); 
-    const [copiedIndex, setCopiedIndex] = useState(null); 
-
-    // * Esta funcion se ejecuta cuando se haga submit, y a la vez ejecuta "fetchApi", es decir realiza la peticion a la API
+    // * Función para validar la URL y enviar una solicitud a la API de TinyURL.
     const handleSubmit = async (e) => {
       e.preventDefault();
+
+      // * Valida si se ha introducido una URL.
       if (inputValue) {
-        // ! API Request
         try {
+          // * Envía una solicitud a la API de TinyURL.
           const response = await fetch(
             `https://tinyurl.com/api-create.php?url=${inputValue}&api_key=${API_KEY}`
           );
-    
+          
+          // * Si la solicitud es correcta, se procesa la respuesta.
           if (response.ok) {
             const data = await response.text();
             const newLink = {
               originalUrl: inputValue,
               tinyUrl: data,
             };
-            setLinks([...links, newLink]); // Añade el nuevo enlace a la lista
+
+            // * Se actualiza el estado con la nueva URL acortada y se añade a la lista de enlaces.
+            setLinks([...links, newLink]); // * Añade el nuevo enlace a la lista
             setTinyUrl(data);
             setButtonTexts((prevButtonTexts) => [...prevButtonTexts, "Copy"]);
           } 
@@ -51,10 +63,13 @@ const Shortening = () => {
           console.error(error);
         }
       } else {
+        // * Se muestra un mensaje de error si no se ha introducido una URL.
         setError("Please add a link");
       }
     };
 
+
+    // * Función para copiar la URL acortada al portapapeles y actualizar el texto del botón de copia.
     const handleCopy = (url, index) => {
       navigator.clipboard
         .writeText(url)
@@ -63,12 +78,13 @@ const Shortening = () => {
           newButtonTexts[index] = "Copied!";
           setButtonTexts(newButtonTexts);
 
-          // Si hay un índice de botón copiado anterior, restaurar su estado
+          // * Si hay un índice de botón copiado anterior, se restaura su estado.
           if (copiedIndex !== null && copiedIndex !== index) {
             newButtonTexts[copiedIndex] = "Copy";
             setButtonTexts(newButtonTexts);
           }
 
+          // * Se actualiza el estado del índice del enlace copiado.
           setCopiedIndex(index);
         })
         .catch((error) => {
@@ -80,7 +96,8 @@ const Shortening = () => {
 
     return (
       <div className={styles.ShorteningMainContainer}>
-        {/* imput */}
+
+        {/* // * Formulario de entrada */}
         <form className={styles.InputAndButtonContainer}>
           <div className={styles.InputContainer}>
             <input
@@ -106,20 +123,28 @@ const Shortening = () => {
           </div>
         </form>
 
+
+        {/* // * Contenedor para todos los enlaces */}
         <div className={styles.AllLinksContainer}>
+          {/* // * Mapear la lista de enlaces y mostrar cada enlace en un contenedor */}
           {links.map((link, index) => (
             <div className={styles.LinkMainContainer}>
+              {/* // * Contenedor para el enlace original */}
               <div className={styles.OriginalLinkContainer}>
                 <p>{link.originalUrl}</p>
               </div>
 
+
+              {/* // * Contenedor para el enlace acortado y el botón de copia */}
               <div className={styles.LinkShortenAndButtonContainer}>
+                {/* // * Contenedor para el enlace acortado */}
                 <div className={styles.LinkShortedContainer}>
                   <a href={link.originalUrl} target="_blank">
                     {link.tinyUrl}
                   </a>
                 </div>
 
+                {/* // *Contenedor para el botón de copia */}
                 <div className={styles.LinksCopyButtonContainer}>
                   <button
                     onClick={() => handleCopy(link.tinyUrl, index)}
